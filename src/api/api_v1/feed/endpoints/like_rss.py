af08bd4d55948b5686afaa6420e562f15fd7c243
Sorkhemiri <sorkhemiri@gmail.com>
2021-12-26 21:18:33 +0330

    feat: issue fixed

--- src/api/api_v1/feed/endpoints/like_rss.py
+++ src/api/api_v1/feed/endpoints/like_rss.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter, Depends
 from starlette.requests import Request
+from starlette.responses import JSONResponse
 
 from entities import User
 from repositories.postgres import UserRepository, LikeRepository
@@ -15,6 +16,13 @@ auth_check = CheckAuthentication(user_repository=UserRepository, user_auth_repos
 
 @router.post("/rss/{rss_id}/like/", tags=["like-rss", "feed"])
 def rss_list(request: Request, rss_id: int, user: User = Depends(auth_check)):
+    request_data = {
+        "rss_id": rss_id,
+        "user": user
+    }
     use_case = LikeRSSUseCase(validator=SubscribeRSSSourceValidator,
                               like_repository=LikeRepository)
-    return use_case.execute(request_model={"rss_id": rss_id, "user": user})
+    data = use_case.execute(request_model=request_data or {})
+    status = data["http_status_code"]
+    del data["http_status_code"]
+    return JSONResponse(content=data, status_code=status)
