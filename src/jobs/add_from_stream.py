3af76b4129907fd0dd6b755f615708225da98b3b
Sorkhemiri <sorkhemiri@gmail.com>
2021-12-27 08:22:40 +0330

    feat: some clean ups

--- src/jobs/add_from_stream.py
+++ src/jobs/add_from_stream.py
@@ -1,24 +1,43 @@
-from datetime import datetime
 import logging
 
 import feedparser
 
-from app import db
 from entities import RSS
+from interfaces.feed_manager_repository_interface import FeedManagerRepositoryInterface
 from repositories.postgres import RSSSourceRepository, RSSRepository
-from repositories.redis import FeedMemory
+from repositories.redis import FeedMemoryRepository, FeedManagerRepository
 from decorators import retry
-from utils import fan_out
 from celery_app import celery_app
+
+
+import datetime
+
+from datetime import datetime
+from typing import List, Type
+
+from repositories.postgres.subscription import SubscriptionRepository
+
+
+def fan_out(key: str, rss_ids: List[int], feed_manager_repository: Type[FeedManagerRepositoryInterface]):
+    now_time = datetime.now()
+    data_values = []
+    for post_id in rss_ids:
+        post_serial_id = f"{int(now_time.timestamp())}{post_id}"
+        data_values.append((post_id, post_serial_id))
+    feed_manager_repository.add_to_channel(key=key, feed=data_values)
+    subscribers = SubscriptionRepository.get_channel_subscriber_by_key(key=key)
+    for subscriber in subscribers:
+        feed_manager_repository.add_to_feed(user_id=subscriber.user.id, feed=data_values)
+        feed_manager_repository.add_to_unseen(user_id=subscriber.user.id, post_ids=rss_ids)
 
 
 @celery_app.task
 @retry(times=3, wait=5)
 def add_from_stream():
-    sources = RSSSourceRepository.get_sources()
+    sources = RSSSourceRepository.get_list()
     for source in sources:
         feed = feedparser.parse(source.link)
-        rss_ids = FeedMemory.get_memory(key=source.key, date=datetime.now())
+        rss_ids = FeedMemoryRepository.get_memory(key=source.key, date=datetime.now())
         stored_rss_ids = []
         stored_post_ids = []
         for item in feed["entries"]:
@@ -43,5 +62,5 @@ def add_from_stream():
                 stored_post_ids.append(created_rss.id)
                 logging.info(f"rss_id --{rss_id}-- saved")
                 stored_rss_ids.append(rss_id)
-        fan_out(key=source.key, rss_ids=stored_post_ids)
-        FeedMemory.add_to_memory(key=source.key, post_ids=stored_rss_ids, date=datetime.now())
+        fan_out(key=source.key, rss_ids=stored_post_ids, feed_manager_repository=FeedManagerRepository)
+        FeedMemoryRepository.add_to_memory(key=source.key, post_ids=stored_rss_ids, date=datetime.now())
--- src/utils/__init__.py
+++ src/utils/__init__.py
@@ -1,4 +1,3 @@
 from .calculate_expiration_interval import calculate_expiration_interval
-# from .fan_out import fan_out
 from .make_password import make_password
 from .make_random_hex import make_random_hex
